c6adbf91cbd445064735f385536b8f94b7b7a710
Jensen <jensenojs@qq.com>
2023-08-22 11:05:22 +0800

    fix focus

--- .config/nvim/lua/plugins/ui/focus.lua
+++ .config/nvim/lua/plugins/ui/focus.lua
@@ -1,36 +1,41 @@
 -- https://github.com/nvim-focus/focus.nvim
 -- Always have a nice view over your split windows
-local ignore_filetypes = { 'NvimTree' }
-local ignore_buftypes = { 'nofile', 'prompt', 'popup' }
+local ignore_filetypes = {'NvimTree'}
+local ignore_buftypes = {'nofile', 'prompt', 'popup'}
 
-local augroup =
-    vim.api.nvim_create_augroup('FocusDisable', { clear = true })
+local augroup = vim.api.nvim_create_augroup('FocusDisable', {
+    clear = true
+})
 
 vim.api.nvim_create_autocmd('WinEnter', {
     group = augroup,
     callback = function(_)
         if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
-            vim.b.focus_disable = true
+            vim.w.focus_disable = true
+        else
+            vim.w.focus_disable = false
         end
     end,
-    desc = 'Disable focus autoresize for BufType',
+    desc = 'Disable focus autoresize for BufType'
 })
 
 vim.api.nvim_create_autocmd('FileType', {
     group = augroup,
     callback = function(_)
         if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
-            vim.b.focus_disable = true
+            vim.w.focus_disable = true
+        else
+            vim.w.focus_disable = false
         end
     end,
-    desc = 'Disable focus autoresize for FileType',
+    desc = 'Disable focus autoresize for FileType'
 })
 
 return {
     'nvim-focus/focus.nvim',
     config = function()
-        require'focus'.setup({
-            excluded_filetypes = {"NvimTree"}
-        })
+        -- require'focus'.setup({
+        --     excluded_filetypes = {"NvimTree"}
+        -- })
     end
 }
