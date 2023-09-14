2dfbb00a01c7c5ee62c7c7741c59b285a8b84b48
Jensen <jensenojs@qq.com>
2023-09-14 11:18:10 +0800

    fix comment can not use in tmux

--- .config/nvim/lua/plugins/ide/comment.lua
+++ .config/nvim/lua/plugins/ide/comment.lua
@@ -1,11 +1,20 @@
 -- https://github.com/terrortylor/nvim-comment
 -- 注释
+local bind = require("utils.bind")
+
+local map_cr = bind.map_cr
+local keymaps = {
+    ["nv|<c-\\>"] = map_cr(":CommentToggle"):with_noremap():with_silent():with_desc("Toggle 注释")
+}
+
+bind.nvim_load_mapping(keymaps)
+
 return {
     "terrortylor/nvim-comment",
     config = function()
+        -- Disable mappings
         require('nvim_comment').setup({
-            line_mapping = "<c-/>",
-            operator_mapping = "<c-?>",
+            create_mappings = false
         })
     end
 }
